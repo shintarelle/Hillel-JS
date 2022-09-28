0f3b881f00f2c600f7c64d08ef043a5e332e2827
AnnRozhkina <annrozhkina@gmail.com>
2022-09-28 22:01:04 +0300

    work on lesson3

--- lessons/lesson3.js
+++ lessons/lesson3.js
@@ -79,3 +79,39 @@ switch (a) {
 }
 
 console.log('-----------------------');
+
+
+// Задача на подумати. Якщо вирішите добре, якщо ні завтра розберемо.
+// function random(min, max, exclude);
+
+// random(1, 100, [3]) // value from 1 to 100 but not include 3, 33, 43, 93
+
+function randomExclude3(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    let randomNum = Math.floor(Math.random() * (max - min)) + min;
+    switch (randomNum) {
+        case 3:
+        case 33:
+        case 43:
+        case 93:
+            break;
+        default:
+            console.log(randomNum);
+    }
+}
+randomExclude3(1, 100);
+
+// random(1, 100, [3,6]) // value from 1 to 100 but not include 3, 6,66,33, 43,46
+
+// сравнение результата в стринговом эквиваленте (поиск в строке 3 или 6) если нашлось то тогда не выводим
+
+// random(1, 100) // value from 1 to 100
+
+function random(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+console.log(random(1, 100));
