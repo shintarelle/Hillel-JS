b71d694b01ccf9747f59ec11d1a54abf408c4c08
AnnRozhkina <annrozhkina@gmail.com>
2022-12-21 23:10:44 +0200

    work on homework 30

--- React/src/App.js
+++ React/src/App.js
@@ -1,27 +1,50 @@
 import { Component } from "react";
-import { renderCurrentType, treeToMap } from "./utils";
-import data from "./data.json";
+import { renderCurrentType, treeToMap, splitPath } from "./utils";
+import data from "./data1.json";
 
 class App extends Component {
-  constructor() {
+
+  constructor(){
     super();
-     // this.map = treeToMap(data[0].children);
+    this.state = {
+      text: '',
+      currentOpenFolder: ["/Common7", "/Common7/IDE"]
+      // currentOpenFolder: ["/Common7",  "/VC", "/Common7/IDE", "/Common7/Tools","/VC/bin"]
+    }
     this.map = treeToMap(data);
+    this.handleSearch = this.handleSearch.bind(this)
   }
 
-   // map = {"/Common7/IDE/msdia140.dll": "msdia140.dll",
-  // "/Common7/Tools/msdia140.dll": "msdia140.dll" }
+  handleSearch(e) {
 
-  render() {
-    console.log(this.map)
+    let arrayValues = Object.values(this.map).filter(item => item.includes(e.target.value))
+    let arrayKeys = [];
+    for(let i = 0; i < arrayValues.length; i++) {
+      let key = Object.keys(this.map).find(key => this.map[key] === arrayValues[i])
+      arrayKeys.push(key)
+    }
+
+    let arrayPath = splitPath(arrayKeys);
+
+    arrayPath = Array.from(new Set(arrayPath)).reverse()
+    console.log(arrayPath)
+
+    let text = e.target.value;
+    this.setState({text: text, currentOpenFolder: arrayPath });
+    console.log('state now', this.state.currentOpenFolder) //! не понимаю почему
+    // состояние уже новое но открытыми остаются папки из стейт по умолчанию
+  }
+
+  render(){
+    // console.log('RENDER APP this.state.currentOpenFolder', this.state.currentOpenFolder)
     return (
+      <>
+      <input placeholder="Seach..." onChange={this.handleSearch}></input>
       <ul>
-        {renderCurrentType(data, ["/Common7",  "/VC", "/Common7/IDE", "/Common7/Tools","/VC/bin"])}
-      </ul>
-    )
+        {renderCurrentType(data, this.state.currentOpenFolder)}
+      </ul></>
+    );
   }
-
-
 }
 
 export default App;
