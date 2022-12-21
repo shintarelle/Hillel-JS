dd27052df38ceac187d0d8090b26078ae5f9955a
AnnRozhkina <annrozhkina@gmail.com>
2022-12-22 01:59:19 +0200

    correct folder React, delete folder

--- React/src/App.js
+++ React/src/App.js
@@ -1,23 +1,17 @@
 import { Component } from "react";
-import { renderCurrentType, treeToMap } from "./utils";
-import data from "./data.json";
 
 class App extends Component {
   constructor() {
     super();
-     // this.map = treeToMap(data[0].children);
-    this.map = treeToMap(data);
+
   }
 
-   // map = {"/Common7/IDE/msdia140.dll": "msdia140.dll",
-  // "/Common7/Tools/msdia140.dll": "msdia140.dll" }
-
   render() {
-    console.log(this.map)
+
     return (
-      <ul>
-        {renderCurrentType(data, ["/Common7",  "/VC", "/Common7/IDE", "/Common7/Tools","/VC/bin"])}
-      </ul>
+      <>
+
+      </>
     )
   }
 
